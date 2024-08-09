65501ae299b450b9c7c9b2435a7ce82b8728ba8a
Tanush Golwala <tanushgolwala@gmail.com>
2024-08-09 19:14:15 +0530

    feat : audio to text

--- App.tsx
+++ App.tsx
@@ -1,10 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import Voice, { SpeechResultsEvent, SpeechErrorEvent } from '@react-native-voice/voice';
 
 export default function App() {
+  const [started, setStarted] = useState<boolean>(false);
+  const [results, setResults] = useState<string[]>([]);
+
+  useEffect(() => {
+    Voice.onSpeechError = onSpeechError;
+    Voice.onSpeechResults = onSpeechResults;
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
+
+  const startSpeechToText = async () => {
+    try {
+      await Voice.start("en-NZ");
+      setStarted(true);
+    } catch (error) {
+      console.error("Error starting speech recognition:", error);
+    }
+  };
+
+  const stopSpeechToText = async () => {
+    try {
+      await Voice.stop();
+      setStarted(false);
+    } catch (error) {
+      console.error("Error stopping speech recognition:", error);
+    }
+  };
+
+  const onSpeechResults = (event: SpeechResultsEvent) => {
+    setResults(event.value ?? []);
+  };
+
+  const onSpeechError = (event: SpeechErrorEvent) => {
+    console.error("Speech recognition error:", event.error);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {!started ? <Button title="Start Speech to Text" onPress={startSpeechToText} /> : undefined}
+      {started ? <Button title="Stop Speech to Text" onPress={stopSpeechToText} /> : undefined}
+      {results.map((result, index) => (
+        <Text key={index}>{result}</Text>
+      ))}
       <StatusBar style="auto" />
     </View>
   );
